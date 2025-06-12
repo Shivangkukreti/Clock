8cda5b1515769ee02d78dfd7dad9b4c5fda5ec55
Shivangkukreti <shivangkukretiwork@gmail.com>
2025-06-13 02:28:03 +0530

    ok

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ clock()
 function clock() {
     clocktime=setInterval(() => {
         const now = new Date();
-    const hours = now.getHours(); 
+    const hours = now.getHours().toString(); 
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
         time.textContent=`${hours}:${minutes}:${seconds}`
